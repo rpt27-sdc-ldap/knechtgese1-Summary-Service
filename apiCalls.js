169f7cd4edb50e2024fd76972364c3745b895bbc
Aschale Siyoum <AschaleS@users.noreply.github.com>
2021-05-03 15:40:17 -0500

    Create apiCalls.js

---
+++ apiCalls.js
@@ -0,0 +1,25 @@
+// here is the url
+`http://localhost:1220/api/summary/${bookId}`
+
+//This is a sample of a data that I'm getting back 
+ [
+    {
+        "_id": `608b721f197222ffe5e1561b`,
+        "id": 10,
+        "__v": 0,
+        "copyright": `©2017 ad nostrud sint (P)2020 minim qui ex"`
+        "short_summary": `Tempor non aute occaecat aute eiusmod in in veniam laborum et. 
+            Consequat occaecat consectetur aute tempor magna magna adipisicing adipisicing. 
+            Commodo magna culpa consequat mollit nulla Lorem magna nisi. Aliqua ullamco Lorem laboris officia 
+            magna ipsum cupidatat dolor aliquip enim nisi fugiat. Enim et ex aute ad cupidatat sint ad laboris 
+            dolore fugiat fugiat excepteur.`,
+        "summary": `Sint in proident dolore do cupidatat sint sunt enim. Adipisicing elit irure veniam tempor 
+            incididunt reprehenderit irure magna dolore consequat adipisicing Lorem reprehenderit mollit commodo.\nConsequat 
+            cillum id exercitation est cupidatat aliquip eu laborum. Reprehenderit nisi cupidatat occaecat anim reprehenderit 
+            nostrud ullamco in eu reprehenderit labore. Id pariatur ad cupidatat ipsum exercitation reprehenderit ex non Lorem 
+            nostrud ullamco. Duis quis enim voluptate ut occaecat id duis reprehenderit. Nisi non deserunt veniam eu exercitation 
+            enim voluptate labore enim qui amet velit aliquip. Esse aute esse magna pariatur nisi duis ex irure labore. 
+            Cupidatat culpa duis qui excepteur sint amet ipsum aliqua consequat amet qui. Magna sint nisi in id pariatur 
+            magna aliqua cupidatat non nostrud proident reprehenderit ad.`
+    }
+]
