638827c1c08441a556a5c1910c759ac9fab0cc32
Aschale <aschaled@gmail.com>
2021-05-07 12:51:42 -0500

    Addedd tests for client components

--- client/src/spec/components/App.test.js
+++ client/src/spec/components/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { configure, shallow, mount, render } from 'enzyme';
+import { configure, shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import "@babel/polyfill";
@@ -10,30 +10,91 @@ import Summary from '../../components/Summary.jsx'
 configure({ adapter: new Adapter() });
 global.fetch = require('jest-fetch-mock');
 
+const mockMath = Object.create(global.Math);
+mockMath.random = () => 0.5;
+global.Math = mockMath;
+
 
 describe("App", () => {
-  it("renders correctly", () => {
+  it("should render App correctly", () => {
     const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
   });
 
 
-  it("includes one div", () => {
+  it("should includes one div", () => {
     const wrapper = shallow(<App />);
     expect(wrapper.find('div').length).toEqual(1);
   });
 
 
-  it("should render Summary", () => {
+  it("should render Summary component", () => {
     const wrapper = shallow(<App />);
     const summary = wrapper.find(Summary);
     expect(summary.exists()).toBe(true);
   });
 
-  it('can fetch response data', async () => {
-    fetchMock.get('http://fake.com', { summary: "Sint in proident dolore do cupidatat sint enim." });
-    const response = await fetch('http://fake.com');
-    const result = await response.json();
-    expect(result.summary).toEqual("Sint in proident dolore do cupidatat sint enim.");
+  const summaries = [
+    {
+      "_id": "608b721f197222ffe5e1561b",
+      "id": 10,
+      "__v": 0,
+      "copyright": "©2017 ad nostrud sint (P)2020 minim qui ex",
+      "short_summary": "Tempor ad cupidatat sint ad laboris dolore fugiat fugiat excepteur.",
+      "summary": "Sint in proident dolore do cupidatat sint sunt consequat adipisicing Lorem reprehenderit mollit commodo."
+    }
+  ];
+  it('should render the Summary component with data correctly', () => {
+    const wrapper = shallow(<Summary summaries={summaries} />)
+    expect(wrapper).toMatchSnapshot();
+
   });
+
+  it('should render empty array when there is no data to map through', () => {
+    const wrapper = shallow(<Summary summaries={[]} />);
+    expect(wrapper).toMatchSnapshot();
+
+  });
+
+  it('should not break with an empty array', () => {
+    const wrapper = shallow(<Summary summaries={[]} />);
+    expect(wrapper.find('summaries')).toHaveLength(0);
+
+  });
+
+  it("should render Summary component", () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.children(Summary).length).toBe(1);
+  });
+
+  it('should fetch the correct data from the api', async () => {
+    const expectedSummary = "Sint in proident dolore do cupidatat sint sunt consequat adipisicing Lorem reprehenderit mollit commodo.";
+    fetchMock.get('http://localhost:1220/api/summary/50', [
+      {
+        "_id": "608b721f197222ffe5e1561b",
+        "id": 50,
+        "__v": 0,
+        "copyright": "©2017 ad nostrud sint (P)2020 minim qui ex",
+        "short_summary": "Tempor ad cupidatat sint ad laboris dolore fugiat fugiat excepteur.",
+        "summary": expectedSummary
+      }
+    ]);
+
+    const wrapper = await mount(<App />);
+    const result = await wrapper.instance();
+
+    await new Promise(res => setTimeout(() => {
+      expect(Array.isArray(result.state.summaries)).toEqual(true);
+      wrapper.update();
+      res()
+    }, 500));
+
+    expect(result.state.summaries.length).toBe(1);
+    //console.log(result.state.summaries);
+    expect(result.state.summaries[0].summary).toEqual(expectedSummary);
+    expect(result.state.summaries[0]).toHaveProperty(`summary`, expectedSummary);
+    //console.log(result.state.summaries);
+    //expect(fetch).toHaveBeenCalledWith('http://example.com')
+  });
+
 });
--- client/src/spec/components/Summary.test.js
+++ client/src/spec/components/Summary.test.js
@@ -5,27 +5,31 @@ import Summary from '../../components/Summary.jsx'
 import BookSummary from '../../components/BookSummary.jsx'
 configure({ adapter: new Adapter() });
 
-describe('Summary', () => {
+describe('Summary component', () => {
   it("should render BookSummary", () => {
   const wrapper = shallow(<Summary summaries={['summary']} />);
   const summary = wrapper.find(BookSummary);
   expect(summary.exists()).toBe(true);
 });
 
-it('Summary renders div', () => {
+it('should render one div', () => {
   const wrapper = shallow(<Summary summaries={['summary']} />);
   expect(wrapper.find('div').length).toEqual(1);
 });
 
-// it('displays summaries if passed props', () => {
-//   const wrapper = shallow(<Summary summaries={['summary']} />);
-//   expect(wrapper.find('BookSummary').exists()).toEqual(true);
-// });
-
-it('should render the state', () => {
-  const wrapper = shallow(<Summary summaries={['summary']} />)
-  const result = wrapper.find('div').text();
-  expect(result).toEqual("<BookSummary />");
+const summaries = [
+  {
+    "_id": "608b721f197222ffe5e1561b",
+    "id": 10,
+    "__v": 0,
+    "copyright": "©2017 ad nostrud sint (P)2020 minim qui ex",
+    "short_summary": "Tempor ad cupidatat sint ad laboris dolore fugiat fugiat excepteur.",
+    "summary": "Sint in proident dolore do cupidatat sint sunt consequat adipisicing Lorem reprehenderit mollit commodo."
+  }
+];
+it('should render the state correctly', () => {
+  const wrapper = shallow(<Summary summaries={summaries}/>)
+  expect(wrapper).toMatchSnapshot();
 
 });
 });
